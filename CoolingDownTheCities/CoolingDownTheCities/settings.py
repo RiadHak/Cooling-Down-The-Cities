44b24bd531f98d73686115f4ffa437325a1e041e
M.Riad Alhakim <89877825+ReiadHa@users.noreply.github.com>
2023-09-26 09:44:49 +0200

    added app to installed app in setting file

--- CoolingDownTheCities/CoolingDownTheCities/settings.py
+++ CoolingDownTheCities/CoolingDownTheCities/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['cdtc.azurewebsites.net']
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard.apps.DashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
