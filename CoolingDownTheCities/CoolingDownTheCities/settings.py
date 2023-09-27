c572f4db7a52e807538eb5c01806535a23238fed
EnesT51 <Enes.t51@outlook.com>
2023-09-27 10:59:56 +0200

    added database configurations

--- CoolingDownTheCities/CoolingDownTheCities/settings.py
+++ CoolingDownTheCities/CoolingDownTheCities/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'CoolingDownTheCities.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cdtc',
+        'USER': 'cdtcadmin',
+        'PASSWORD': 'Projectroot.',
+        'HOST': 'cdtc-project.mysql.database.azure.com',
+        'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- CoolingDownTheCities/dashboard/models.py
+++ CoolingDownTheCities/dashboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.core.validators import validate_email, validate_slug
 # Create your models here.
+class CustomUser(models.Model):
+    username  = models.CharField(max_length=25)
+    email     = models.EmailField(validators=[validate_email])
+    password  = models.CharField(max_length=50)
+    
+    
+    
