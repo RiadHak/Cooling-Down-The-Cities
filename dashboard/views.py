65c65889124230d1fe6d6c119bc73cd58ce1c5b4
EnesT51 <Enes.t51@outlook.com>
2023-10-01 00:48:46 +0200

    refactord/moddified the code so its uses the validation method

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterFrom
+from django.contrib import messages
+from .validators import validation
+
+
 def login(request):
     
     return render(request, 'login.html')
-
+    
+    
 def signup(request):
     
     if request.method == "POST":
         form = RegisterFrom(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/login')
+            messages.success(request, 'Registration successful!')
+            return redirect('/signup')
         else:
             form = RegisterFrom()
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            checking = validation(username, email)
+            _= [messages.error(request, x) for x in checking]              
             return render(request, 'signup.html', {'form':form})
-        
+
     form = RegisterFrom()
     return render(request, 'signup.html', {'form':form})
-
-
 
 def privatePolicy(request):
     
