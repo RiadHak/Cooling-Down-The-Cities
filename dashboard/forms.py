0f9700f9c07c3d001e085d2ac790bc1ae384d0e0
EnesT51 <Enes.t51@outlook.com>
2023-10-01 00:35:08 +0200

    cleaned unused imports

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,21 +1,17 @@
 from django import forms
 from .models import Register
+from django.core.exceptions import ValidationError
+
+
 class RegisterFrom(forms.ModelForm):
     username = forms.CharField(max_length=20, required=True, widget=forms.TextInput(
-        attrs={
-            'class': 'form-control',
-        } 
-    ))
+        attrs={'class': 'form-control'}))
+    
     email = forms.EmailField(required=True, widget=forms.EmailInput(
-        attrs={
-            'class': 'form-control',
-        }   
-    ))
+        attrs={'class': 'form-control'}))
+    
     password = forms.CharField(required=True, widget=forms.PasswordInput(
-        attrs={
-            'class': 'form-control',
-        }
-    ))
+        attrs={'class': 'form-control'}))
     class Meta:
         model = Register
         fields = ('username', 'email', 'password')
