91c4d69bbc5dc476c6c4e56544c0f130c57e2557
EnesT51 <Enes.t51@outlook.com>
2023-10-01 00:34:17 +0200

    cleaned unused imports

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-
-from .validators import *
 # Create your models here.
 class Register(models.Model):
-    username  = models.CharField(max_length=25, unique=True)
+    username  = models.CharField(max_length=25,unique=True)
     email     = models.EmailField(unique=True)
     password  = models.CharField(max_length=50)     
     class Meta:
