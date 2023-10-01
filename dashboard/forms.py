9e75bc14d5588c4719282c1e38211fc39f472585
EnesT51 <Enes.t51@outlook.com>
2023-10-01 22:49:24 +0200

    added validations

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,20 +1,30 @@
 from django import forms
 from .models import Register
 from django.core.exceptions import ValidationError
+from django.core import validators
+from django.core.validators import validate_email
 
 
 class RegisterFrom(forms.ModelForm):
-    username = forms.CharField(max_length=20, required=True, widget=forms.TextInput(
-        attrs={'class': 'form-control'}))
+    username = forms.CharField(
+        validators=[validators.MinLengthValidator(5), validators.MaxLengthValidator(25)], 
+        required=True, 
+        widget= forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'username'}))
     
-    email = forms.EmailField(required=True, widget=forms.EmailInput(
-        attrs={'class': 'form-control'}))
+    email = forms.EmailField(
+        required=True, 
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'email'}))
     
-    password = forms.CharField(required=True, widget=forms.PasswordInput(
-        attrs={'class': 'form-control'}))
+    password = forms.CharField(
+        validators=[validators.MinLengthValidator(5)], 
+        required=True, 
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'password'}))
     class Meta:
         model = Register
         fields = ('username', 'email', 'password')
+        
+    
+        
         
         
     
