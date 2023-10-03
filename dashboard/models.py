f014ddf6308a164214ffcc46e613a7147f29e83c
EnesT51 <Enes.t51@outlook.com>
2023-10-03 18:04:37 +0200

    needed to change column length otherwise the hashed password was to long

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.core import validators
 # Create your models here.
 class Register(models.Model):
     username  = models.CharField(max_length=25,unique=True)
     email     = models.EmailField(unique=True)
-    password  = models.CharField(max_length=50)     
+    password  = models.CharField(max_length=100)     
     class Meta:
         db_table = 'users'
