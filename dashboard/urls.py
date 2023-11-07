44fab535cf025dfb5d0021f5244cdab85a6833d5
EnesT51 <Enes.t51@outlook.com>
2023-11-07 19:49:12 +0100

    small changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.signin, name='login'),
+    path('signin/', views.signin, name='signin'),
     path('logout/', views.signout, name='signout'),
     path('signup/', views.signup, name='signup'),
     path('privatepolicy/', views.privatePolicy, name='privatepolicy'),
