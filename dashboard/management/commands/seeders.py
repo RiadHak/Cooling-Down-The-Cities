eca6c9c7657d7e0c05e7bba6143236a51100cb67
EnesT51 <Enes.t51@outlook.com>
2023-11-08 10:49:53 +0100

    1 + toegevoegd bij elke value zodat je ook de max value kan krijgen

--- dashboard/management/commands/seeders.py
+++ dashboard/management/commands/seeders.py
@@ -7,11 +7,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         
         for _ in range(100):
-            temp = random.uniform(-20, 50)
-            co2 = random.uniform(300, 5000)
-            Luchtdruk = random.uniform(940, 1060)
-            Luchtvochtigheid = random.uniform(0, 100)
-            Luchtkwaliteit = random.uniform(1, 5)
+            temp = random.uniform(-20, 50 +1)
+            co2 = random.uniform(300, 5000 +1)
+            Luchtdruk = random.uniform(940, 1060 +1)
+            Luchtvochtigheid = random.uniform(0, 100 +1)
+            Luchtkwaliteit = random.uniform(1, 5 +1)
             Seeders.objects.create(
                 temperatuur = temp,
                 CO2 = co2,
