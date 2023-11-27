140d818c148614d327a8153b3f24bceb0188b24d
Bilal094 <89781477+Bilal094@users.noreply.github.com>
2023-11-27 10:35:39 +0100

    Revert "Fix layout"

--- dashboard/static/js/temp.js
+++
@@ -1,9 +0,0 @@
-let tempValue = 10;
-
-let value = tempValue* 2.64;
-document.getElementById("span1").innerHTML = tempValue;
-
-
-document.getElementById("temparature-meter__inner-ring").innerHTML += "<style>#temparature-meter__inner-ring::after{transform: rotatez(" + (value) + "deg);}</style>";
-
-
